e0063330d3d708630102e031d44dae970d5bdf06
Kaan Yıldız <hipnoz_50@hotmail.com>
2024-04-18 19:57:02 +0300

    Java_1.java comitted

--- Java_1.java
+++ Java_1.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class AddtionQuiz {
 	public static void main(String[] args) {
-		System.out.println("Denemes");
+		System.out.println("Denemesen");
+		System.out.println("iyi olur");
+		System.out.println("sanki");
 	}
 }
